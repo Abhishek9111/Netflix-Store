c2fca17ca27520e1020bfd9a2a100ffd2d080399
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-15 01:05:04 +0530

    Routed home screen to add button

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text,StyleSheet,ScrollView} from 'react-native'
+import { Fab,Icon } from 'native-base'
 
-const Home = ()=>{
+const Home = ({navigation, route})=>{
     return(
-        <Text>Home</Text>
+        <ScrollView contentContainerStyle = {styles.container}>
+          <Text>
+            List of Season Goes here
+          </Text>
+          <Fab
+          style={{backgroundColor:'#5067FF'}}
+          position ="bottomRight"
+          onPress={()=>navigation.navigate('Add')}
+          >
+            <Icon name='add'/>
+          </Fab>
+        </ScrollView>
     )
 }
 
