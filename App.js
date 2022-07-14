2ea918e54fa9a00b88ae19a4e393ec9ab4797432
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-15 00:32:22 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//screens
+import Home from './Screens/Home'
+import Add from './Screens/Add'
+import Edit from './Screens/Edit'
+
+
+
+
+const App =()=>{
+  return(
+    <>
+    <Text>Hello</Text>
+    </>
+  )
+}
+
+export default App;
