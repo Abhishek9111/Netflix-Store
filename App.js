350d3d2658fe2c900f3bf48a53fd3b994008405b
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-15 00:49:02 +0530

    Functional and UI changes

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-
+import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -19,13 +9,67 @@ import Home from './Screens/Home'
 import Add from './Screens/Add'
 import Edit from './Screens/Edit'
 
-
+const Stack = createStackNavigator()
 
 
 const App =()=>{
   return(
     <>
-    <Text>Hello</Text>
+    <NavigationContainer>
+      
+      
+      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen
+        name = 'Home'
+        component={Home}
+        options={{
+          headerStyle:{
+            backgroundColor:"0F4C75"
+          },
+          title: 'Abhi Netflix App',
+          headerTitleStyle:{
+            textAlign: 'center',
+            color: '00b7c2'
+          }
+        }}
+        >
+        </Stack.Screen>
+      
+      <Stack.Screen
+        name = 'Add'
+        component={Add}
+        options={{
+          headerStyle:{
+            backgroundColor:"0F4C75"
+          },
+          title: 'Abhi Netflix App',
+          headerTitleStyle:{
+            textAlign: 'center',
+            color: '00b7c2'
+          }
+        }}
+        >
+        </Stack.Screen>
+      
+      </Stack.Navigator>
+      <Stack.Screen
+        name = 'Edit'
+        component={Edit}
+        options={{
+          headerStyle:{
+            backgroundColor:"0F4C75"
+          },
+          title: 'Abhi Netflix App',
+          headerTitleStyle:{
+            textAlign: 'center',
+            color: '00b7c2'
+          }
+        }}
+        >
+        </Stack.Screen>
+
+
+    </NavigationContainer>
     </>
   )
 }
